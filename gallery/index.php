8391b5fef638222c07b2f86accb0234c161cc4cf
knigamaxim <maxwpdev@gmail.com>
2020-03-09 02:57:40 +0200

    update plugin structure, add start scripts

---
+++ gallery/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: Photo Gallery
+ * Description: Photo Gallery plugin for templates. Allow to add and to show your best photos and creative work
+ * Version: 1.0
+ * Author: Maxim Kniga
+ **/
+
+require_once dirname(__DIR__) . '/gallery.php';
